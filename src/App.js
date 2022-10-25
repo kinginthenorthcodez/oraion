5ed68bf3a83862a88ce9b6c16fa878a352883034
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-10-26 01:23:21 +0530

    Add Splash

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 //import PropsType from "./1-PropTypes/Proptypes";
 import Index from './pages';
 import Friends from './1-props/Prop';
 import ContextAPI from './1-contextAPI/contextAPI';
 import { useSelector, useDispatch } from 'react-redux';
 import { increament, decreament, isLogged } from './actions';
+import PacmanLoader from 'react-spinners/PacmanLoader';
+import ClockLoader from 'react-spinners/ClockLoader';
 
 // import { reducer } from "./1-useReducer/reducer";
 
 const App = () => {
   const counter = useSelector((state) => state.counts);
   const islogged = useSelector((state) => state.isLoggedIn);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
+  // Custom css for loader
+  const override = `
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
   const dispatch = useDispatch();
   console.log(islogged);
-  return (
-    <>
+  return isLoading ? (
+    <PacmanLoader
+      color={'#36d7b7'}
+      isLoading={isLoading}
+      css={override}
+      size={150}
+    />
+  ) : (
+    <div>
+      <ClockLoader
+        color={'#36d7b7'}
+        isLoading={isLoading}
+        css={override}
+        size={150}
+      />
       <h2>Hooks </h2>
       <h3>Counter: {counter}</h3>
       {islogged ? (
@@ -37,7 +66,7 @@ const App = () => {
       <h2>Props Example</h2>
       <Friends />
       <ContextAPI />
-    </>
+    </div>
   );
 };
 
